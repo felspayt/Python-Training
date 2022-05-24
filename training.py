88bbaaff7004d78bdc927d17e9992cc8a19bf17a
felspayt <feliksp@tce.loc>
2022-05-24 16:50:04 +0400

    added new file

---
+++ training.py
@@ -0,0 +1,8 @@
+a = int(input("Please input firts integer: "))
+b = int(input("Please input second integer: "))
+print ('Product equals to', a * b)
+print ('Sum equals to', a + b)
+print ('Subtract equals to', a - b)
+print ('Division equals to', a / b)
+print ('Remainder or Modulus equals to', a % b)
+print ('Exponentiation or Modulus equals to', a ** b)
